a2f706e266c7b96a501507e3b7291fbb23633dbe
Justin Becker <jab7yp@virginia.edu>
2019-02-09 14:52:18 -0500

    Slerp Derp
    Slerping Script

---
+++ Dillow/Assets/Scripts/Slerp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slerp : MonoBehaviour
+{
+    public float smooth_speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Face(GameObject target)
+    {
+        Vector3 direction =  (target.transform.position - transform.position).normalized;
+        transform.rotation = Quaternion.Slerp(transform.rotation,
+        Quaternion.LookRotation(direction), smooth_speed);
+    }
+}
