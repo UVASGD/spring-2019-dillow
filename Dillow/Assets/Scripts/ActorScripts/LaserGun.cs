6267da480a9bb9e125cf19b01465bb59019ca9f8
Andrew-64 <31630789+Andrew-64@users.noreply.github.com>
2019-02-16 16:05:52 -0500

    LaserGun

---
+++ Dillow/Assets/Scripts/ActorScripts/LaserGun.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserGun : MonoBehaviour
+{
+    public float range;
+    public float aimForce;
+
+    public GameObject barrel;
+
+    public Rigidbody rb;
+    LineRenderer lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = barrel.GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Aim(GameObject target)
+    {
+        Vector3 diff = (target.transform.position - barrel.transform.position).normalized;
+        rb.AddForceAtPosition(diff * aimForce, barrel.transform.position);
+    }
+
+    public void Fire()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(barrel.transform.position, barrel.transform.forward, out hit, range))
+        {
+            Hit(hit.collider.gameObject, hit.point, hit.normal);
+        }
+
+        
+
+        lineRenderer.SetPositions();
+    }
+
+    public void Hit(GameObject go, Vector3 position, Vector3 normal)
+    {
+        if(CompareTag("Ground"))
+        {
+            //Dink
+        }
+        else if(CompareTag("Player"))
+        {
+            //Ouch
+        }
+    }
+}
