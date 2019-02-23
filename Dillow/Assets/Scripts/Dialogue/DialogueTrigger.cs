c00b524d759a246b6bbe195652d562c8c072da11
wesdaug23 <wcd5f@virginia.edu>
2019-02-23 14:36:17 -0500

    dialogteststart

---
+++ Dillow/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(SphereCollider))]
+public class DialogueTrigger : MonoBehaviour
+{
+	SphereCollider sphere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		sphere = GetComponent<SphereCollider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnTriggerEnter2D(Collider other) {
+		
+	}
+}
