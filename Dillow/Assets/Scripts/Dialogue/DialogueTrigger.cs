d8e9e6fa2129399c8c045e19accfa7e8d0ba5c58
Simon Whittle <smw2ed@virginia.edu>
2019-02-23 15:50:35 -0500

    Old Hedgehog Dialogue Start

--- Dillow/Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Dillow/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -1,26 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fungus;
 
-//[RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(SphereCollider))]
 public class DialogueTrigger : MonoBehaviour
 {
 	SphereCollider sphere;
+
+    public Flowchart chart;
 
     // Start is called before the first frame update
     void Start()
     {
 		sphere = GetComponent<SphereCollider>();
-
+        sphere.isTrigger = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter(Collider other) {
+        GameObject go = other.gameObject;
+
+        if (go.CompareTag("Player") && BallController.interact == 2) {
+            DoTrigger();
+        }
     }
 
-	void OnTriggerEnter2D(Collider other) {
-		
-	}
+    private void OnTriggerStay(Collider other) {
+        GameObject go = other.gameObject;
+
+        if (go.CompareTag("Player") && BallController.interact == 2) {
+            DoTrigger();
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+
+        GameObject go = other.gameObject;
+
+        if (go.CompareTag("Player")) {
+            chart.ExecuteBlock("EndTrigger");
+        }
+    }
+
+    void DoTrigger() {
+
+        if (chart != null) {
+            chart.ExecuteBlock("Trigger");
+        }
+    }
 }
