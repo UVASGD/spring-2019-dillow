82f7ca2c2e66f558dd919930e27befbead446efc
Nicholas O\'Connor <nbo5td@virginia.edu>
2019-03-03 15:57:23 -0500

    Updated Mob to include Collide method

--- Dillow/Assets/Scripts/Actors/Hypervisors/Mob.cs
+++ Dillow/Assets/Scripts/Actors/Hypervisors/Mob.cs
@@ -6,7 +6,9 @@ public class Mob : Body
 {
 
     Ragdoll ragdoll;
-
+    
+    //Minimum magnitude of the impact for a mob to die
+    public int impactThresh;
 
     // Start is called before the first frame update
     protected override void Start()
@@ -22,9 +24,17 @@ public class Mob : Body
         damager.Damage(Vector3.up);
     }
 
-    // Update is called once per frame
-    void Update()
+    
+
+    public override void Collide(List<Tag> tags = null, TagHandler t = null,
+        Vector3? direction = null, Vector3? impact = null)
     {
-        
+        Vector3 dir = (Vector3)((direction == null) ? Vector3.up : direction);
+        Vector3 imp = (Vector3)((impact == null) ? Vector3.zero : impact);
+
+        if (t.HasTag(Tag.Player) && imp.magnitude >= impactThresh)
+        {
+            Die();
+        }
     }
 }
