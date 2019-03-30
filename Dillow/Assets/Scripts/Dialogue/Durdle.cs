bf07b83590b861be036afe8fc6fdb9cecb82a3dc
Simon Whittle <smw2ed@virginia.edu>
2019-03-29 22:30:23 -0400

    Durdle

--- Dillow/Assets/Scripts/Dialogue/Durdle.cs
+++ Dillow/Assets/Scripts/Dialogue/Durdle.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Durdle : MonoBehaviour
 {
+
+    AudioSource source;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        source = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        source.volume = (Vector3.Angle(transform.up, Vector3.up) / 180f);
+
+
     }
+
+
 }
