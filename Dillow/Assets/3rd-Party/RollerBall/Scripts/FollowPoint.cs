7df343ddd1b063804469b09976ced006c7133832
Wyatt Joyner <wcj3te@virginia.edu>
2019-03-31 10:25:11 -0400

    fixed prefab

---
+++ Dillow/Assets/3rd-Party/RollerBall/Scripts/FollowPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPoint : MonoBehaviour {
+
+    Transform target;
+
+    Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+        target = transform.parent;
+        transform.SetParent(null);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.SmoothDamp(transform.position, target.position, ref velocity, 0.02f);
+	}
+}
