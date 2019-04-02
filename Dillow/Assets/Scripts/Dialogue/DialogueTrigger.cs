5aa9ff0f0bcfd71bca281fc89d3d3b20067f26ab
Wyatt Joyner <wcj3te@virginia.edu>
2019-04-02 03:49:55 -0400

    updating dialogue trigger

--- Dillow/Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Dillow/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -3,59 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fungus;
 
-[RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(Collider))]
 public class DialogueTrigger : MonoBehaviour
 {
-	SphereCollider sphere;
-    ParticleSystem talkPrompt;
+    ParticleSystem interactPrompt;
 
     public Flowchart chart;
 
     // Start is called before the first frame update
     void Start()
     {
-		sphere = GetComponent<SphereCollider>();
-        sphere.isTrigger = true;
+		GetComponent<Collider>().isTrigger = true;
+        interactPrompt = GetComponent<ParticleSystem>();
 
-        talkPrompt = GetComponent<ParticleSystem>();
-        //Debug.Log("talkPrompt: " + talkPrompt.ToString());
-        if (talkPrompt != null && ! talkPrompt.isStopped) {
-            talkPrompt.Stop();
+        if (interactPrompt != null && ! interactPrompt.isStopped) {
+            interactPrompt.Stop();
         }
     }
 
     private void ShowPrompt() {
-        if (talkPrompt != null) {
-            talkPrompt.Play();
+        if (interactPrompt != null) {
+            interactPrompt.Play();
         }
     }
 
     private void HidePrompt() {
-        if (talkPrompt != null) {
-            talkPrompt.Stop();
-            talkPrompt.Clear();
+        if (interactPrompt != null) {
+            interactPrompt.Stop();
+            interactPrompt.Clear();
         }
     }
 
     private void OnTriggerEnter(Collider other) {
-        GameObject go = other.gameObject;
+        TagHandler t = other.GetComponent<TagHandler>();
 
-        bool tooFast = DillowController.instance.gameObject.GetMainRigidbody().velocity.magnitude > 0.5f;
-
-        if (go.CompareTag("Player") && DillowController.interact == 2 && !tooFast)
-        { // callcontroller.interact == 2 checks whether 'e' has been pressed
-            DoTrigger();
-        }
-        else if (go.CompareTag("Player")) {
+        if (t && t.HasTag(Tag.Player)) {
             ShowPrompt();
         }
     }
 
     private void OnTriggerStay(Collider other) {
-        GameObject go = other.gameObject;
+        TagHandler t = other.GetComponent<TagHandler>();
 
-        if (go.CompareTag("Player") && DillowController.interact == 2)
-        { // callcontroller.interact == 2 checks whether 'e' has been pressed
+        if ((t && t.HasTag(Tag.Player)) && DillowController.interact == 2)
+        { 
             DoTrigger();
         }
     }
@@ -75,20 +66,19 @@ public class DialogueTrigger : MonoBehaviour
             chart.ExecuteBlock("Trigger");
             //string chatType = chart.GetStringVariable("type");
             DillowController.instance.can_input = false;
-            DillowController.instance.gameObject.GetMainRigidbody().isKinematic = true;
+            DillowController.instance.body.rb.isKinematic = true;
         }
     }
 
     void EndTrigger() {
         if (chart != null) {
             chart.ExecuteBlock("EndTrigger");
-            DillowController.instance.can_input = true;
-            DillowController.instance.gameObject.GetMainRigidbody().isKinematic = false;
+            FinishDialogue();   
         }
     }
 
     public void FinishDialogue() {
         DillowController.instance.can_input = true;
-        DillowController.instance.gameObject.GetMainRigidbody().isKinematic = false;
+        DillowController.instance.body.rb.isKinematic = false;
     }
 }
