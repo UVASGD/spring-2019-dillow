6e0e6fcad90e4681b997158e996ea840a8df376b
Julián Kristofer McClinton <jaykmcclinton@gmail.com>
2019-04-02 19:37:14 -0400

    laser party cat fight sleepover

--- Dillow/Assets/Scripts/Actors/LaserGun.cs
+++ Dillow/Assets/Scripts/Actors/LaserGun.cs
@@ -1,13 +1,12 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserGun : MonoBehaviour
-{
+public class LaserGun : MonoBehaviour {
     [HideInInspector] public float range = 500;
 
     public GameObject barrel;
-    public LineRenderer laser;
+    public LineRenderer aim_laser;
 
     float aimAngleThreshold = 75f;
     Vector3 aimDirection;
@@ -17,21 +16,18 @@ public class LaserGun : MonoBehaviour
     float laser_time = 0.25f;
     bool can_fire = true;
 
-    public LineRenderer aim_laser;
     public bool activated;
 
+    [Header("FX")]
     public GameObject shot_fx;
-    public GameObject hit_fx;
+    //public GameObject hit_fx;
     public GameObject charge_fx;
-    float charge_time = 1f; 
+    float charge_time = 1f;
     bool charging;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         aimDirection = barrel.transform.forward;
-        laser.enabled = false;
-        laser.useWorldSpace = true;
         aim_laser.enabled = false;
         aim_laser.useWorldSpace = true;
 
@@ -40,66 +36,54 @@ public class LaserGun : MonoBehaviour
         //InvokeRepeating("HipFire", 1f, 1f);
     }
 
-    void Update()
-    {
-        if (activated)
-        {
+    void Update() {
+        if (activated) {
             aim_laser.SetPosition(0, barrel.transform.position);
 
             RaycastHit hit;
-            if (Physics.Raycast(barrel.transform.position, aimDirection, out hit, range, Physics.AllLayers, QueryTriggerInteraction.Ignore))
-            {
+            if (Physics.Raycast(barrel.transform.position, aimDirection, out hit, range, Physics.AllLayers, QueryTriggerInteraction.Ignore)) {
                 aim_laser.SetPosition(1, hit.point);
-            }
-            else
-            {
+            } else {
                 aim_laser.SetPosition(1, barrel.transform.position + (aimDirection * range));
             }
         }
     }
 
-    public void Activate(bool act = true)
-    {
+    public void Activate(bool act = true) {
         activated = act;
         aim_laser.enabled = act;
     }
 
-    public bool Aim(GameObject target)
-    {
+    public bool Aim(GameObject target) {
         Vector3 targetDir = (target.transform.position - barrel.transform.position).normalized;
         float angleDiff = Vector3.Angle(barrel.transform.forward, targetDir);
-        if (angleDiff < aimAngleThreshold)
-        {
+        if (angleDiff < aimAngleThreshold) {
             aimDirection = targetDir;
             return true;
-        }
-        else
-        {
+        } else {
             aimDirection = barrel.transform.forward;
             return false;
-        }      
+        }
     }
 
-    public void HipFire()
-    {
+    public void HipFire() {
         aimDirection = barrel.transform.forward;
         Fire();
     }
 
-    public void Fire(GameObject target = null)
-    {
+    void OnParticleCollision(GameObject other) {
+        print("COLDOE");
+    }
+
+    public void Fire(GameObject target = null) {
         if (!can_fire)
             return;
 
-        if (target)
-        {
+        if (target) {
             TagHandler t = target.GetComponent<TagHandler>();
-            if (t && t.HasTag(Tag.Dashing))
-            {
+            if (t && t.HasTag(Tag.Dashing)) {
                 aimDirection = (target.transform.position - (target.GetComponent<Rigidbody>().velocity / 10f)) - barrel.transform.position;
-            }
-            else
-            {
+            } else {
                 aimDirection = target.transform.position - barrel.transform.position;
             }
         }
@@ -108,53 +92,38 @@ public class LaserGun : MonoBehaviour
 
         if (shot_fx) Fx_Spawner.instance.SpawnFX(shot_fx, barrel.transform.position, barrel.transform.forward);
 
-        laser.SetPosition(0, barrel.transform.position);
+        barrel.transform.LookAt(target.transform);
 
         RaycastHit hit;
-        if (Physics.Raycast(barrel.transform.position, aimDirection, out hit, range, Physics.AllLayers, QueryTriggerInteraction.Ignore))
-        {
-            laser.SetPosition(1, hit.point);
+        if (Physics.Raycast(barrel.transform.position, aimDirection, out hit, range, Physics.AllLayers, QueryTriggerInteraction.Ignore)) {
             Hit(hit.collider.gameObject, hit.point, hit.normal);
-            if (hit_fx) Fx_Spawner.instance.SpawnFX(hit_fx, hit.point, hit.normal);
-        }
-        else
-        {
-            laser.SetPosition(1, barrel.transform.position + (barrel.transform.forward * range));
+            //if (hit_fx) Fx_Spawner.instance.SpawnFX(hit_fx, hit.point, hit.normal);
         }
 
         StartCoroutine(LaserFlash());
     }
 
-    IEnumerator LaserFlash()
-    {
+    IEnumerator LaserFlash() {
         can_fire = false;
-        laser.enabled = true;
         yield return new WaitForSeconds(laser_time);
-        laser.enabled = false;
         //yield return new WaitForSeconds(laserCD);
         can_fire = true;
     }
 
-    public void Hit(GameObject hit, Vector3 position, Vector3 normal)
-    {
-        if (hit.CompareTag("Ground"))
-        {
-        }
-        else if(hit.GetComponent<Body>())
-        {
+    public void Hit(GameObject hit, Vector3 position, Vector3 normal) {
+        if (hit.CompareTag("Ground")) {
+        } else if (hit.GetComponent<Body>()) {
             hit.GetComponent<Body>().Collide(position, hit_tags, direction: -normal);
         }
     }
 
-    public void Charge(float aim_time)
-    {
+    public void Charge(float aim_time) {
         if (can_fire && !charging && aim_time < charge_time) StartCoroutine(ChargeCo());
     }
 
-    IEnumerator ChargeCo()
-    {
+    IEnumerator ChargeCo() {
         charging = true;
-        if (charge_fx) Fx_Spawner.instance.SpawnFX(charge_fx, barrel.transform.position, barrel.transform.forward);
+        if (charge_fx) Fx_Spawner.instance.SpawnFX(charge_fx, barrel.transform.position, barrel.transform.forward, parent: transform);
         yield return new WaitForSeconds(charge_time);
         charging = false;
     }
---
+++ Dillow/Assets/Scripts/JULIAN/EmissionController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class EmissionController : MonoBehaviour {
+
+    [SerializeField]
+    public float intensity = 1;
+    private Material m;
+
+    // Start is called before the first frame update
+    void Start() {
+        Renderer r = GetComponent<MeshRenderer>();
+        m = r.material;
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //m.SetColor("_EmissionColor", m.GetColor("EmissionColor") * intensity);
+    }
+}
