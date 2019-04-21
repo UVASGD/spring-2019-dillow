5490897c1a798c705c78fab7042db344e1b024b5
Simon Whittle <smw2ed@virginia.edu>
2019-04-21 16:42:38 -0400

    messy dialogue shit.
    Haven't quite finished this, but want to be able to figure out a sorta-static thing for flowchart variables.

---
+++ Dillow/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class DialogueManager : MonoBehaviour
+{
+    private static DialogueManager _instance;
+    public static DialogueManager instance {
+        get {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<DialogueManager>();
+            }
+            return _instance;
+        }
+    }
+
+    public Flowchart flowchart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flowchart = GetComponent<Flowchart>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
