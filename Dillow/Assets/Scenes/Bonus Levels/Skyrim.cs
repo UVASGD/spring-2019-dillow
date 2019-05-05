dfafc60901feb587cefa9825501b70e901f00873
Wyatt Joyner <wcj3te@virginia.edu>
2019-05-05 13:33:30 -0400

    added skyrim, fixed dialogue cam

---
+++ Dillow/Assets/Scenes/Bonus Levels/Skyrim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Skyrim : MonoBehaviour
+{
+    VideoPlayer v;
+    bool started;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        v = GetComponent<VideoPlayer>();
+        v.targetCamera = Camera.main;
+        FadeController.FadeInCompletedEvent += Play;
+        v.started += delegate { started = true; };
+    }
+
+    void Play()
+    {
+        v.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (started && !v.isPlaying)
+        {
+            print("oof");
+            Destroy(gameObject);
+        }
+    }
+
+
+}
