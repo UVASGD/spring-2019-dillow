27392f7912df183feaee6b45839569f317452489
Wyatt Joyner <wcj3te@virginia.edu>
2019-05-05 02:17:23 -0400

    imported fungus encounters

---
+++ Dillow/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+[RequireComponent(typeof(Flowchart))]
+public class DialogueManager : MonoBehaviour
+{
+    private static DialogueManager _instance;
+    public static DialogueManager instance {
+        get {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<DialogueManager>();
+            }
+            return _instance;
+        }
+    }
+
+    public Flowchart flowchart;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        flowchart = GetComponent<Flowchart>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
